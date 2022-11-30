8a5dae2530e76dce19a858dc82f137ffef01ca07
waking <waking.zy@gmail.com>
2022-11-30 17:06:35 +0800

    excel_read引入sqlite结果入库

--- excel_read/src/line95.rs
+++
@@ -1,8 +0,0 @@
-pub struct line95{
-    pub url: String,
-    pub line95: f32
-}
-
-impl line95 {
-    
-}
--- excel_read/src/main.rs
+++ excel_read/src/main.rs
@@ -1,81 +1,93 @@
-use calamine::{Reader, open_workbook, Xlsx, Rows, DataType};
-use std::{collections::HashMap};
+mod helper_sql;
+mod line_limit;
 
-fn filter_line95(rows: Rows<'_, DataType>) -> HashMap<String, f32> {
-    let mut map_line95:HashMap<String, f32> = HashMap::new();
+use calamine::{open_workbook, Reader, Xlsx};
+use helper_sql::{build_insert_sql, build_update_sql};
+use line_limit::{filter_timelimit, L99Interface};
+use rusqlite::{params, Connection, Result};
 
-    let mut _rows = rows.filter(|row| {
-        let _line95 = row[9].get_float().unwrap() as f32;
-        return _line95 > 1.5;
-    });
-
-    return map_line95;
-}
-
-fn print_l95() {
-    let path = "C:/Users/Administrator/Downloads/leopard.xlsx";
+fn analyze_limit() {
+    let path = "C:/Users/waking/Downloads/fortunecat-w48.xlsx";
+    let week: usize = 48;
+    let application = String::from("fortuncat");
+    let limit_second = 2.0;
+    let limit_col_num: usize = 6;
+    // 打开文件
     let mut _workbook: Xlsx<_> = open_workbook(path).expect("cannot open file!");
+    let sheet = _workbook
+        .worksheet_range_at(0)
+        .expect("未找到可用的sheet")
+        .unwrap();
+    let vec_timelimit =
+        filter_timelimit(sheet.rows(), limit_col_num, limit_second, week, application.as_str());
+    let timelimit_count = vec_timelimit.len();
+    let mut update_count: usize = 0;
 
-    if let Some(Ok(range)) = _workbook.worksheet_range("msku维度数据"){
-        let total_cells = range.get_size().0 * range.get_size().1;
-        let non_empty_cells: usize = range.used_cells().count();
-        println!("Found {} cells in 'msku维度数据', including {} non empty cells", total_cells, non_empty_cells);
-    }
+    // 连接sqlite并入库
+    let conn = Connection::open("slow_interface.sqlite").unwrap();
+    for l99_interface in vec_timelimit {
+        print!(
+            "url: {}, L99: {}, application: {}",
+            l99_interface.path, l99_interface.l99_time, l99_interface.application
+        );
+        // 查询是否存在
+        let mut stmt = conn
+            .prepare("select * from interface where path = ? and week = ?")
+            .unwrap();
+        let rs = stmt.query_row(params![l99_interface.path, week], |row| {
+            // println!("数据信息{}", (row.get(1) as Result<String>).unwrap());
+            return Ok(L99Interface {
+                id: row.get(0).unwrap(),
+                path: row.get(1).unwrap(),
+                l99_time: row.get(2).unwrap(),
+                week: row.get(3).unwrap(),
+                application: row.get(4).unwrap(),
+            }) as Result<L99Interface>;
+        });
 
-    // let sheet_name = "statistics";
-    // let sheet = _workbook.worksheet_range(&sheet_name).expect(&format!( "未找到目标sheet: {}",sheet_name));
-    let sheet = _workbook.worksheet_range_at(0).expect("未找到可用的sheet");
-    let mut count_row: usize = 0;
-    let mut url_map:HashMap<String, f64> = HashMap::new();
-    // 遍历sheet得到url + L95的map
-    for row in sheet.unwrap().rows() {
-        count_row += 1;
-        // if count_row == 1 {
-        //     continue;
-        // }
-        // print!("本行数据：");
-        // println!("row={:?}, row[0]={:?}", row, row[0]);
-        // 大于1.5s的才记录
-        let value: f64 = row[9].get_float().unwrap_or(0.0);
-        if value <= 1.5 {
-            continue;
+        let exists;
+        if rs.is_ok() {
+            exists = rs.unwrap();
+        } else {
+            exists = L99Interface::default();
         }
-        let mut key = row[0].to_string();
-        if key.ends_with("/") {
-            let rindex = key.rfind("/").unwrap();
-            // println!("第{}行,最后一个\"/\"出现的位置{}", count_row, rindex);
-            let _key = key.split_at(rindex);
-            key = _key.0.to_string();
-            // println!("截取后的url:{}",key);
-        }
-        if url_map.contains_key(&key){
-            if url_map.get(&key).unwrap().lt(&value) {
-                url_map.insert(key, value);
+        let sql_str: String;
+        if exists.id > 0 {
+            println!(", 已存在: w{}", week);
+            // 如果同一个week中接口已经存在,则比较L99时间大小,新的大就更新
+            if exists.l99_time >= l99_interface.l99_time {
+                continue;
+            } else {
+                sql_str = build_update_sql(exists.id, &l99_interface.l99_time, application.as_str());
             }
-        }else{
-            url_map.insert(key, value);
+        } else {
+            // 拼接插入sql
+            sql_str = build_insert_sql(&l99_interface.path, &l99_interface.l99_time, week, application.as_str());
         }
+
+        // println!("{}", sql_str);
+        // update_count = conn.execute(&sql_str, ()).unwrap() + 1;
+        match conn.execute(&sql_str, ()) {
+            Ok(_o) => {
+                update_count += 1;
+                print!(", 插入成功: w{}", week);
+            }
+            Err(e) => {
+                println!(", 插入失败, {}", e);
+                continue;
+            }
+        };
+        println!();
     }
-    println!("一共{}行数据", count_row);
-    let mut count_greater:usize = 0;
-    // 输出所有url
-    let mut _keys = Vec::from_iter(url_map.keys());
-    // _keys.sort();
-    _keys.sort_by(|a, b| a.to_lowercase().cmp(&b.to_lowercase()));
-    for key in _keys {
-        println!("url: {},L95: {}", key, url_map.get(key).unwrap());
-        count_greater += 1;
-    };
-    // 输出L95大于1.5s的url以及L95时间
-    // for entry in url_map {
-    //     if entry.1 > 1.5 {
-    //         count_greater += 1;
-    //         println!("url: {},L95: {}", entry.0, entry.1);
-    //     }
-    // }
-    println!("大于1.5s的有{}行",count_greater);
+    println!(
+        "共{}个接口,超过阈值({}s)的接口{}个,插入{}条数据",
+        sheet.rows().len(),
+        limit_second,
+        timelimit_count,
+        update_count
+    );
 }
 
 fn main() {
-    print_l95();
+    analyze_limit();
 }
